d23a1f8fabbd108ec29206b990c4d0a01815ff39
varunkarmikanda-codecraft <varun.karmikanda@codecraft.co.in>
2026-03-31 20:16:53 +0000

    add initial implementation of interaction and validator managers

---
+++ src/presentation/interaction-manager.ts
@@ -0,0 +1,58 @@
+import * as readline from 'node:readline';
+import { stdin as input, stdout as output } from 'node:process'
+import type { ValidatorFn } from '../core/validators/validator.types.js';
+
+
+export interface AskOptions {
+    defaultAnswer?: string | undefined;
+    validator?: ValidatorFn | undefined;
+}
+
+export interface Choice {
+    label: string;
+    value: string;
+}
+
+export const openInteractionManager = () => {
+
+    const rl = readline.createInterface({ input, output });
+    
+    const ask = (question: string, options?: AskOptions): Promise<string | undefined> => {
+        const { defaultAnswer, validator } = options ?? {};
+        return new Promise((resolve) => {
+            rl.question(defaultAnswer ? `${question} (${defaultAnswer}) ` : `${question} `, (answer: string) => {
+                if(validator && !validator(answer)) {
+                    console.log(`Invalid input. Please try again!`);
+                    return resolve(ask(question, { defaultAnswer: defaultAnswer, validator: validator }));
+                }
+                resolve(answer || defaultAnswer);
+            })
+        })
+    }
+    
+    const choose: (question: string, choices: Choice[], optional?: boolean) => Promise<Choice | undefined> = async (question, choices, optional) => {
+        console.log(question);
+        choices.forEach((choice) => {
+            console.log(`${choice.label}. ${choice.value}`);
+        })
+        const choice = await ask('Please enter your choice: ', {
+            validator: (input) => {
+                if(!optional && input.trim() === "") {
+                    return true;
+                }
+                return choices.some(choice => choice.value === input);
+            }
+        })
+        return choices.find(c => c.value === choice)
+    }
+    
+    const close = () => {
+        rl.close();
+    }
+
+    return {
+        ask,
+        choose,
+        close,
+    }
+}
